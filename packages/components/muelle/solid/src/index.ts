0f090282909f640a56db9d33b328a13fd40eae63
kazviz <137312421+kazviz@users.noreply.github.com>
2026-01-04 23:51:44 +0700

    feat(muelle): add solid implementation entry

---
+++ packages/components/muelle/solid/src/index.ts
@@ -0,0 +1,11 @@
+/**
+ * @packageDocumentation
+ * Solid implementation entry for the `muelle` organ-level footer.
+ *
+ * @remarks
+ * Re-exports public types and the default `Muelle` component view.
+ * Consumers should import from this module for IntelliSense-rich types
+ * and the main component.
+ */
+export * from "./muelle.types"
+export { default } from "./muelle.view"
